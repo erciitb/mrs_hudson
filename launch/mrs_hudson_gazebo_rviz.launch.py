dc934ecdffd535ef976f46b2d9a557826e3d51da
Saurabh Gupta <saurabhgupta0342@gmail.com>
2023-12-22 22:46:06 +0530

    v1.1

--- launch/mrs_hudson_gazebo_rviz.launch.py
+++ launch/mrs_hudson_gazebo_rviz.launch.py
@@ -14,8 +14,8 @@ def generate_launch_description():
 	pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
 	sdf_file = os.path.join(pkg_project, 'models', 'mrs_hudson.urdf')
 	with open(sdf_file, 'r') as infp:
- 		robot_desc = infp.read()
- 	
+ 		robot_desc = infp.read()			
+ 		
 	robot_state_publisher = Node(
   	 package='robot_state_publisher',
    	 executable='robot_state_publisher',
@@ -25,20 +25,34 @@ def generate_launch_description():
    	     {'use_sim_time': True},
    	     {'robot_description': robot_desc},
    	 ]
-	)
-	
+	) 			
+
 	gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(pkg_project, 'launch', 'mrs_hudson_empty_world.launch.py'))
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
+        launch_arguments={'gz_args': os.path.join(pkg_project, 'worlds', 'mrs_hudson.sdf')}.items(),
     	)
+	
+	bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
+                   '/model/mrs_hudson/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry',
+                   '/world/empty/model/mrs_hudson/joint_state@sensor_msgs/msg/JointState@ignition.msgs.Model',
+                   'lidar@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan',
+                   ],
+        output='screen'
+        )
+        
 	rviz = Node(
 	package='rviz2',
 	executable='rviz2',
 	arguments=['-d', os.path.join(pkg_project, 'config', 'mrs_hudson.rviz')],
-	)
+	)        
 
 	return LaunchDescription([
 		robot_state_publisher,
-		rviz,
-		gz_sim
+		rviz,	            
+		gz_sim,
+		bridge
 		])
